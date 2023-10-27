e64083c9b7b429bda732e61989bd93708545f48e
sds704 <146737667+sds704@users.noreply.github.com>
2023-10-27 20:01:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {StateProvider } from './utils/StateProvider';
-import reducer from './utils/reducer';
-import { initialState } from './utils/reducer';
+import Reducer from './utils/Reducer';
+import { initialState } from './utils/Reducer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
+    <StateProvider initialState={initialState} reducer={Reducer}>
     <App />
     </StateProvider>
   </React.StrictMode>
